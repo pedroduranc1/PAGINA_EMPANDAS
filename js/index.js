a1111fee2cfede6a41eac4a175d7a67463cd98fc
pedroduranc1 <61203515+pedroduranc1@users.noreply.github.com>
2023-05-18 16:49:26 -0400

    FORMULARIO DE COMPRA COMPLETO

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
 let clienteslist = document.getElementById("clienteslist")
 let empanadaslist = document.getElementById("empandaslist")
+let empandalist = document.getElementById("empandalist")
 let titleEmpanda = document.getElementById("titleEmpanda")
 let btnAction = document.getElementById("btnAction")
+
+let empanadasClient = document.getElementById("empanadasClient")
 
 let formStatus = false
 
 Clientes = []
 Empandas = []
+let EmpanadasCliente = []
 
 const obtenerEmpanada = (e) => {
     e.preventDefault()
@@ -58,6 +62,26 @@ const obtenerEmpanada = (e) => {
 let formAgregarEmpanada = document.getElementById("formAgregarEmpanada")
 formAgregarEmpanada.addEventListener("submit",obtenerEmpanada)
 
+const generarCompra = (e) => {
+    e.preventDefault()
+
+    let ID_EMPANADA = EmpanadasCliente.map(empanada=> {return empanada.id })
+
+    let CLIENTE_COMPRA = {
+        data:{
+            EMPANDAS_COMPRADAS: Number(e.target["CANTIDAD"].value),
+            cliente: Number(clienteslist.value),
+            empandas: ID_EMPANADA
+        }
+    }
+
+    //SUBIR LA INFO A LA BD
+    registrarCompra(CLIENTE_COMPRA)
+}
+
+let formCrearCompra = document.getElementById("formCrearCompra")
+formCrearCompra.addEventListener("submit",generarCompra)
+
 const GetClientes = async () => {
     try {
         const respuesta = await fetch('http://localhost:1337/api/clientes')
@@ -75,6 +99,7 @@ const GetEmpanadas = async () => {
         const datos = await respuesta.json()
         Empandas = [...datos.data]
         empanadaslist.innerHTML = ""
+        empandalist.innerHTML = ""
         mostrarEmpanadas()
     } catch (error) {
         console.log(error)
@@ -98,6 +123,13 @@ const mostrarEmpanadas = () => {
             <Option value="${empanada.id}">${empanada.attributes.GUISO}</Option>
         `
     });
+
+    Empandas.forEach(empanda=>{
+        empandalist.innerHTML +=
+        `
+            <Option value="${empanda.id}">${empanda.attributes.GUISO}</Option>
+        `
+    })
 }
 
 const agregarEmpanada = (data) => {
@@ -171,6 +203,51 @@ const hazAgregar = () => {
     formAgregarEmpanada.reset()
 }
 
+const listarEmpanda = () => {
+    let id = empandalist.value
+    let EMPANADA_ENCONTRADA = Empandas.find(empanda => empanda.id === Number(id))
+    let EMPANADA_INFO = {
+        id: EMPANADA_ENCONTRADA.id,
+        ...EMPANADA_ENCONTRADA.attributes
+    }
+
+    let isListed = EmpanadasCliente.find(empanada => empanada.id === EMPANADA_INFO.id)
+    if(isListed === undefined){
+        EmpanadasCliente.push(EMPANADA_INFO)
+        empanadasClient.innerHTML = ""
+        mostrarEmpandasCliente()
+    }
+    
+}
+
+const mostrarEmpandasCliente = () => {
+    EmpanadasCliente.forEach(empanada=> {
+        empanadasClient.innerHTML+= 
+        `
+        <div class="w-full bg-white place-content-center py-4 px2 cursor-pointer rounded"
+        onclick="eliminarSeleccion()"
+        ><h1 class="text-center font-bold">${empanada.GUISO}</h1></div>
+        `
+    })
+    
+}
+
+const registrarCompra = (data) => {
+    fetch('http://localhost:1337/api/compras', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            data
+        })
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log("COMPRAR REALIZADA")
+        })
+        .catch(err => console.log(err))
+}
 
 GetClientes()
 GetEmpanadas()
